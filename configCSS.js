4a65a3b4e91c02d01553ec2c95d44b9b049ead2f
Th <116724740+ThiagoAbreuBR@users.noreply.github.com>
2023-03-18 00:04:52 -0300

    Update configCSS.js
    readline por function, presença da question no console.log, cores chamativas por chalk, todas possibilidades de escrita da palavra SAIR, e  array da propriedades CSS sendo imprimida uma abaixo da outra, ao invés em linha no console.log.

--- configCSS.js
+++ configCSS.js
@@ -1,17 +1,26 @@
-import readlineSync from "readline"
+import readline from "readline"
 import chalk from "chalk";
 
-var rl = readlineSync.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-let configCSS = [];
+function propertyCSS() {
 
-rl.on('line', (addConfigCSS) => {
-  configCSS.push(addConfigCSS)
-  if (addConfigCSS === "SAIR") {
-    console.log((chalk.red)("Propriedades CSS adicionadas\n") + (chalk.yellow)(configCSS.sort()));
-    rl.close()
-  }
-});
+  let configCSS = [];
+
+  let reportCSS = rl.question = "Digite uma propriedade de CSS ou SAIR"
+  console.log(chalk.hex('#FF8800').bold(reportCSS))
+
+  rl.on('line', (addConfigCSS) => {
+    configCSS.push(addConfigCSS)
+    
+    if (addConfigCSS.toUpperCase() === "SAIR") {
+      configCSS.pop()
+      console.log((chalk.hex("#FF0000").bold)("Propriedades CSS adicionadas\n") + (chalk.hex("#FFFF00").bold)(configCSS.sort().join('\n')));
+      rl.close()
+    }
+  });
+}
+propertyCSS()
